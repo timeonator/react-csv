bf067fa71e5bf3a192a210e32f49754dcd1188a2
timeonator <timeonator@gmail.com>
2021-06-09 06:48:49 -0700

    initial checking

---
+++ src/csv-reader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Papa from 'papaparse'
+import Table from 'react-table'
+
+export default function CsvReader() {
+  const [rows, setRows] = React.useState([])
+  React.useEffect(() => {
+    async function getData() {
+      const response = await fetch('/data/nodes.csv')
+      const reader = response.body.getReader()
+      const result = await reader.read() // raw array
+      const decoder = new TextDecoder('utf-8')
+      const csv = decoder.decode(result.value) // the csv text
+      const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
+      const rows = results.data // array of objects
+      setRows(rows)
+    }
+    getData()
+  }, []) // [] means just do this once, after initial render
+  return (
+    <div className="app">
+      <Table cols={tripColumns} rows={rows} />
+    </div>
+  )
+}
