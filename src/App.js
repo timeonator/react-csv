976e4fa85ec1f8df2b927927b9a1d0009a5dbf7d
timeonator <timeonator@gmail.com>
2021-06-11 20:03:23 -0700

    removed empty header section since it breaks the UI

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import SampleTable from './sample-table'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
       <SampleTable />
     </div>
   );
