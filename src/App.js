cf55f19ac1eb2015ad250be9efd48ef625cdfb1f
timeonator <timeonator@gmail.com>
2021-06-14 10:47:21 -0700

    added DataPackage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import SampleTable from './sample-table'
 import DownloaderDemo from './downloader-demo'
+import DataPackage from './forms/datapackage'
 
 function App() {
   return (
     <div className="App">
+      <DataPackage />
       <SampleTable />
       <DownloaderDemo />
     </div>
