a686b9a64556bd044930af67da805d7a4ec67b5e
timeonator <timeonator@gmail.com>
2021-09-08 14:24:01 -0700

    Just an html encoded table component

--- src/a-table.js
+++ src/a-table.js
@@ -2,16 +2,18 @@ import './App.css';
 const ATable = () => {
   return (
     <table className="myTable">
-      <tr>
-        <td>a</td>
-        <td>b</td>
-        <td>c</td>
-      </tr>
-      <tr>
-        <td>d</td>
-        <td>e</td>
-        <td>f</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>a</td>
+          <td>b</td>
+          <td>c</td>
+        </tr>
+        <tr>
+          <td>d</td>
+          <td>e</td>
+          <td>f</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
